21d04dfd0380e1b3d77b0c8366e7fb6db21dce0b
Parv <parvvj2000@gmail.com>
2025-02-18 11:18:14 +0530

    changes

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -3,19 +3,13 @@ import 'package:flutter/material.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: AppConstants.fontFamily,
-<<<<<<< HEAD
   primaryColor: const Color(0xFF0c7372),
   secondaryHeaderColor: const Color(0xFF0c7372),
-=======
-  primaryColor: const Color(0xFF9675c4),
-  secondaryHeaderColor: const Color(0xFFefe6fc),
->>>>>>> f6ba2fcd8c522daa8299b79f7fa3c59ce7266449
   brightness: Brightness.dark,
   cardColor: const Color(0xFF29292D),
   hintColor: const Color(0xFFE7F6F8),
   focusColor: const Color(0xFFC3CAD9),
   shadowColor: Colors.black.withOpacity(0.4),
-<<<<<<< HEAD
   popupMenuTheme: const PopupMenuThemeData(
       color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
@@ -31,21 +25,4 @@ ThemeData dark = ThemeData(
     onSurface: Colors.white70,
     shadow: Colors.black.withOpacity(0.4),
   ),
-=======
-  popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
-  dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
-  colorScheme: ColorScheme(
-    brightness: Brightness.dark,
-    primary: const Color(0xFF9675c4),
-    onPrimary: const Color(0xFF9675c4),
-    secondary: const Color(0xFFefe6fc),
-    onSecondary: const Color(0xFFefe6fc),
-    error: Colors.redAccent,
-    onError: Colors.redAccent,
-    surface: Colors.white10,
-    onSurface:  Colors.white70,
-    shadow: Colors.black.withOpacity(0.4),
-  ),
-
->>>>>>> f6ba2fcd8c522daa8299b79f7fa3c59ce7266449
 );
